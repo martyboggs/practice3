d66b560a486c2a575adcbca23bd1b3fa1640efe1
martyboggs <whack47@gmail.com>
2020-02-10 15:20:05 -0800

    camera follow

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MainCamera : MonoBehaviour
 {
+    private Vector3 pos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pos.Set(Player.instance.transform.position.x, 10, Player.instance.transform.position.z - 30);
+        transform.position = Vector3.MoveTowards(transform.position, pos, 0.1f);
     }
 }
