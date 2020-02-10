07c7f399d0abb3c7d3bbf976776f290ca49628d3
martyboggs <whack47@gmail.com>
2020-02-10 15:05:59 -0800

    movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,9 +10,9 @@ public class Player : MonoBehaviour
     public Leg RightLeg;
     private Leg ActiveLeg;
     private Leg InactiveLeg;
-    public Vector3 direction = Vector3.forward;
-    private Vector3 lastPosition;
     private float velocity = 0.2f;
+    public Vector3 direction;
+    private Vector3 desiredDirection;
 
     private void Awake()
     {
@@ -29,14 +29,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = Vector3.Normalize(transform.position - lastPosition);
-        lastPosition = transform.position;
+        desiredDirection = ArrowKeys();
 
-        ArrowKeys();
+        direction = Vector3.RotateTowards(direction, desiredDirection, 0.3f, 0.1f);
 
+        // facing direction has a delay
+        transform.LookAt(transform.position + direction);
+
+        // move in direction of keys
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a") || Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d") ||
+        Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w") || Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s")) {
+            transform.Translate(desiredDirection * velocity, Space.World);
+        }
+
+        // set legs to hips
         LeftLeg.rend.SetPosition(0, transform.position + 0.5f * Vector3.left);
         RightLeg.rend.SetPosition(0, transform.position + 0.5f * Vector3.right);
 
+        // take step
         if (ActiveLeg.planted && InactiveLeg.planted && Vector3.Distance(transform.position, ActiveLeg.rend.GetPosition(1)) > 2)
         {
             TakeStep(ActiveLeg);
@@ -57,23 +67,25 @@ public class Player : MonoBehaviour
         leg.startPosition = leg.rend.GetPosition(1);
     }
 
-    void ArrowKeys()
+    Vector3 ArrowKeys()
     {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
-            transform.Translate(Vector3.left * velocity, Space.World);
+            desiredDirection += Vector3.left;
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
-            transform.Translate(Vector3.right * velocity, Space.World);
+            desiredDirection += Vector3.right;
         }
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
-            transform.Translate(Vector3.forward * velocity, Space.World);
+            desiredDirection += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
-            transform.Translate(Vector3.back * velocity, Space.World);
+            desiredDirection += Vector3.back;
         }
+        desiredDirection = Vector3.Normalize(desiredDirection);
+        return desiredDirection;
     }
 }
